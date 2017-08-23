8b0621212a4af7bf6d5b699339c4c45c748e75bb
Yichuan Shen <shen.yichuan@gmail.com>
2017-08-23 21:55:41 +0200

    Fix arrow label positioning

--- src/components/GridEdge.js
+++ src/components/GridEdge.js
@@ -123,7 +123,7 @@ export default class GridEdge extends Component {
             let newHeight = height * Math.abs(Math.cos(angle)) + width * Math.abs(Math.sin(angle))
 
             this.setState({
-                labelX: `calc(50% + ${-width / 2}px)`,
+                labelX: `calc(50% + ${-width / 2 - 6.5}px)`,
                 labelY: (this.props.alt ? bbox.y + bbox.height : bbox.y - height)
                     + (+!!this.props.alt * 2 - 1) * ((newHeight - height) / 2 + 5)
             })
