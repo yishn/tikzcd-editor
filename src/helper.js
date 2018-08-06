554bdcbc31251e3b55cabb577edfe2b047e62c58
Younesse Kaddar <ykaddar@ens-paris-saclay.fr>
2018-08-06 20:04:40 +0100

    Support of unicode in the encoding

--- src/helper.js
+++ src/helper.js
@@ -23,3 +23,17 @@ export function arrScale(m, a) {
 export function arrSubtract(a, b) {
     return a.map((x, i) => x - b[i])
 }
+
+// to support unicode
+
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode(parseInt(p1, 16))
+    }))
+}
+
+function b64DecodeUnicode(str) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+    }).join(''))
+}
