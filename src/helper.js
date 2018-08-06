c7a11dd4d1797a17f27fdc4fc833b00d87032e0b
Younesse Kaddar <ykaddar@ens-paris-saclay.fr>
2018-08-06 20:12:05 +0100

    Added "export"

--- src/helper.js
+++ src/helper.js
@@ -26,13 +26,13 @@ export function arrSubtract(a, b) {
 
 // to support unicode
 
-function b64EncodeUnicode(str) {
+export function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
         return String.fromCharCode(parseInt(p1, 16))
     }))
 }
 
-function b64DecodeUnicode(str) {
+export function b64DecodeUnicode(str) {
     return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
     }).join(''))
