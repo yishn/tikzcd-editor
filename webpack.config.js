bcdf7cbe9dcbf5cf9fdc46846fec1363cdfb1037
Yichuan Shen <shen.yichuan@gmail.com>
2020-01-21 22:59:57 +0100

    Format with prettier on save

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path')
 
 module.exports = (env, argv) => ({
-    entry: './src/index.js',
+  entry: './src/index.js',
 
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
 
-    devtool: argv.mode === 'production' ? false : 'cheap-module-eval-source-map',
+  devtool: argv.mode === 'production' ? false : 'cheap-module-eval-source-map',
 
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {
-                            modules: false,
-                            targets: "defaults"
-                        }]
-                    ],
-                    plugins: [
-                        ['@babel/plugin-proposal-class-properties', {
-                            loose: true
-                        }],
-                        ['@babel/transform-react-jsx', {
-                            pragma: 'h',
-                            pragmaFrag: 'Fragment'
-                        }]
-                    ]
-                }
-            },
-        }]
-    },
-
-    resolve: {
-        alias: {
-            'preact': path.join(__dirname, 'node_modules/preact/dist/preact.min')
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              modules: false,
+              targets: "defaults"
+            }]
+          ],
+          plugins: [
+            ['@babel/plugin-proposal-class-properties', {
+              loose: true
+            }],
+            ['@babel/transform-react-jsx', {
+              pragma: 'h',
+              pragmaFrag: 'Fragment'
+            }]
+          ]
         }
+      },
+    }]
+  },
+
+  resolve: {
+    alias: {
+      'preact': path.join(__dirname, 'node_modules/preact/dist/preact.min')
     }
+  }
 })
