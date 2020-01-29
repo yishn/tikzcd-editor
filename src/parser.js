36d6ea2105c060f95047fe808374672820f76317
Yichuan Shen <shen.yichuan@gmail.com>
2020-01-29 20:12:57 +0100

    Use doken

--- src/parser.js
+++ src/parser.js
@@ -1,76 +1,4 @@
-function regexRule(regex) {
-  return contents => {
-    let match = regex.exec(contents)
-    if (match == null) return null
-
-    return match[0].length
-  }
-}
-
-function createTokenizer(rules, options = {}) {
-  return (contents, opts = {}) => {
-    opts = {...options, ...opts}
-
-    let {shouldStop = token => false} = opts
-    let tokens = []
-    let [row, col, pos] = [0, 0, 0]
-
-    while (contents.length > 0) {
-      let token = null
-
-      for (let [type, rule] of rules) {
-        let length = rule(contents)
-        if (length == null) continue
-
-        let value = contents.slice(0, length)
-
-        token = {
-          type,
-          value,
-          row,
-          col,
-          pos
-        }
-
-        break
-      }
-
-      if (token == null) {
-        let value = contents[0]
-
-        token = {
-          type: 'invalid',
-          value,
-          row,
-          col,
-          pos
-        }
-      }
-
-      if (token.type[0] !== '_') tokens.push(token)
-      if (shouldStop(token)) break
-
-      // Update source position
-
-      let newlineIndices = Array.from(token.value)
-        .map((c, i) => (c === '\n' ? i : null))
-        .filter(x => x != null)
-
-      row += newlineIndices.length
-
-      if (newlineIndices.length > 0) {
-        col = token.value.length - newlineIndices.slice(-1)[0] - 1
-      } else {
-        col += token.value.length
-      }
-
-      pos += token.value.length
-      contents = contents.slice(token.value.length)
-    }
-
-    return tokens
-  }
-}
+import {regexRule, createTokenizer} from 'doken'
 
 export function parseLabel(contents) {
   if (contents[0] !== '"') return null
@@ -133,59 +61,55 @@ export function parseNode(contents) {
   }
 }
 
-export const tokenizeArrow = createTokenizer(
-  [
-    ['_whitespace', regexRule(/^\s+/)],
-    ['_comma', regexRule(/^,/)],
-    ['command', regexRule(/^\\arrow\[/)],
-    ['end', regexRule(/^\]/)],
-    ['alt', regexRule(/^'/)],
-    ['argName', regexRule(/^([a-zA-Z]+ )*[a-zA-Z]+/)],
-    ['argValue', regexRule(/^=\d+(em)?/)],
-    [
-      'label',
-      contents => {
+export const tokenizeArrow = createTokenizer({
+  rules: [
+    regexRule('_whitespace', /^\s+/),
+    regexRule('_comma', /^,/),
+    regexRule('command', /^\\arrow\[/),
+    regexRule('end', /^\]/),
+    regexRule('alt', /^'/),
+    regexRule('argName', /^([a-zA-Z]+ )*[a-zA-Z]+/),
+    regexRule('argValue', /^=\d+(em)?/),
+    {
+      type: 'label',
+      match: contents => {
         let label = parseLabel(contents)
         if (label == null) return null
 
-        return label.match.length
+        return {length: label.match.length}
       }
-    ]
+    }
   ],
-  {
-    shouldStop: token => ['invalid', 'end'].includes(token.type)
-  }
-)
+  shouldStop: token => [null, 'end'].includes(token.type)
+})
 
-export const tokenize = createTokenizer(
-  [
-    ['_whitespace', regexRule(/^\s+/)],
-    ['_comment', regexRule(/^%.*/)],
-    ['begin', regexRule(/^\\begin{tikzcd}/)],
-    ['end', regexRule(/^\\end{tikzcd}/)],
-    [
-      'node',
-      contents => {
+export const tokenize = createTokenizer({
+  rules: [
+    regexRule('_whitespace', /^\s+/),
+    regexRule('_comment', /^%.*/),
+    regexRule('begin', /^\\begin{tikzcd}/),
+    regexRule('end', /^\\end{tikzcd}/),
+    {
+      type: 'node',
+      match: contents => {
         let {match} = parseNode(contents)
-        return match.length === 0 ? null : match.length
+        return match.length === 0 ? null : {length: match.length}
       }
-    ],
-    [
-      'arrow',
-      contents => {
+    },
+    {
+      type: 'arrow',
+      match: contents => {
         if (!contents.startsWith('\\arrow[')) return null
 
-        let tokens = tokenizeArrow(contents)
+        let tokens = [...tokenizeArrow(contents)]
         let lastToken = tokens[tokens.length - 1]
         if (lastToken.type !== 'end') return null
 
-        return lastToken.pos + 1
+        return {length: lastToken.pos + 1}
       }
-    ],
-    ['align', regexRule(/^&/)],
-    ['newrow', regexRule(/^\\\\/)]
+    },
+    regexRule('align', /^&/),
+    regexRule('newrow', /^\\\\/)
   ],
-  {
-    shouldStop: token => ['invalid', 'end'].includes(token.type)
-  }
-)
+  shouldStop: token => [null, 'end'].includes(token.type)
+})
--- tests/parser.test.js
+++ tests/parser.test.js
@@ -52,9 +52,11 @@ t.test('parseLabel', async t => {
 
 t.test('tokenizeArrow', async t => {
   t.test('tokenize basic arrow', async t => {
-    let tokens = parser.tokenizeArrow(
-      '\\arrow[rr, "hi", hook\', bend left, shift left=2]'
-    )
+    let tokens = [
+      ...parser.tokenizeArrow(
+        '\\arrow[rr, "hi", hook\', bend left, shift left=2]'
+      )
+    ]
     let tokenTypes = tokens.map(token => token.type)
     let tokenValues = tokens.map(token => token.value)
 
@@ -83,9 +85,11 @@ t.test('tokenizeArrow', async t => {
   })
 
   t.test('tokenize loops', async t => {
-    let tokens = parser.tokenizeArrow(
-      '\\arrow["f"\', loop, distance=2em, in=305, out=235]'
-    )
+    let tokens = [
+      ...parser.tokenizeArrow(
+        '\\arrow["f"\', loop, distance=2em, in=305, out=235]'
+      )
+    ]
     let tokenTypes = tokens.map(token => token.type)
     let tokenValues = tokens.map(token => token.value)
 
@@ -118,11 +122,13 @@ t.test('tokenizeArrow', async t => {
   })
 
   t.test('tokenize invalid token', async t => {
-    let tokens = parser.tokenizeArrow(
-      '\\arrow["f"\', loop, distance=2em, in=, out=235]'
-    )
+    let tokens = [
+      ...parser.tokenizeArrow(
+        '\\arrow["f"\', loop, distance=2em, in=, out=235]'
+      )
+    ]
 
-    let invalidToken = tokens.find(token => token.type === 'invalid')
+    let invalidToken = tokens.find(token => token.type == null)
 
     t.ok(invalidToken != null)
     t.equal(invalidToken.value, '=')
@@ -130,19 +136,21 @@ t.test('tokenizeArrow', async t => {
 })
 
 t.test('tokenize', async t => {
-  let tokens = parser.tokenize(
-    `% This is a comment
-    \\begin{tikzcd}
-    A \\ \\arrow[d, "\\overline{g}"'] &  & A\\times B \\arrow[rrdd, "g"]
-      \\arrow[rr, "\\pi_2"] \\arrow[ll, "\\pi_1"']
-      \\arrow[dd, "\\overline{g}\\times\\mathbf{1}_B" description, dashed] &
-      & B\\% \\arrow[d, "\\mathbf{1}_B"] \\\\
-    \\{C^B\\} &  &  &  & {B} % This is a comment
-      \\\\
-    &  & C^B\\times B \\arrow[llu, "\\pi'_1"] \\arrow[rru, "\\pi'_2"']
-      \\arrow[rr, "{\\mathrm{ev}_{B,C}}"'] &  & C
-    \\end{tikzcd}`
-  )
+  let tokens = [
+    ...parser.tokenize(
+      `% This is a comment
+      \\begin{tikzcd}
+      A \\ \\arrow[d, "\\overline{g}"'] &  & A\\times B \\arrow[rrdd, "g"]
+        \\arrow[rr, "\\pi_2"] \\arrow[ll, "\\pi_1"']
+        \\arrow[dd, "\\overline{g}\\times\\mathbf{1}_B" description, dashed] &
+        & B\\% \\arrow[d, "\\mathbf{1}_B"] \\\\
+      \\{C^B\\} &  &  &  & {B} % This is a comment
+        \\\\
+      &  & C^B\\times B \\arrow[llu, "\\pi'_1"] \\arrow[rru, "\\pi'_2"']
+        \\arrow[rr, "{\\mathrm{ev}_{B,C}}"'] &  & C
+      \\end{tikzcd}`
+    )
+  ]
 
   let tokenTypes = tokens.map(token => token.type)
   let nodes = tokens
